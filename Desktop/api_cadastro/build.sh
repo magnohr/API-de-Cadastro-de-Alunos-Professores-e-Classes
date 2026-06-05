4c369d626d4b0448b97626a557f5b4fc1eac4d1c
Imperador das galaxias <magnohenriquereis@gmail.com>
2026-06-05 13:44:22 -0300

    ajuste 2

--- Desktop/api_cadastro/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-pip install -r requirements.txt
-
-python school_api/manage.py collectstatic --no-input
-python school_api/manage.py migrate
--- Desktop/api_cadastro/school_api/teachers/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Teacher
-
-class TeacherSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Teacher
-        fields = ['id', 'first_name', 'last_name', 'email', 'specialty', 'created_at']
-        read_only_fields = ['id', 'created_at']
